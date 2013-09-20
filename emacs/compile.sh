239ba7bd2e77418de5c3a303a8e8530a68160586
Leonardo Cecchi <leonardoce@interfree.it>
2013-09-20 22:14:50 +0000

    getit script for emacs

--- emacs/compile.sh
+++ emacs/compile.sh
@@ -3,6 +3,7 @@ tar xvf emacs-24.3.tar.xz
 cd emacs-24.3
 
 # Configure and compile
+tce-load -i gtk2-dev
 ./configure --prefix=/usr/local --with-gif=no
 make -j3
 make DESTDIR=/tmp/emacs install
---
+++ emacs/getit.sh
@@ -0,0 +1,1 @@
+wget http://ftp.gnu.org/gnu/emacs/emacs-24.3.tar.xz
