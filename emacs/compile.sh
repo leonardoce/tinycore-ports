f2d8e35276de96adc95842b109a531755a1f2d76
Leonardo Cecchi <leonardoce@interfree.it>
2013-09-20 08:40:47 +0000

    compilazione emacs

---
+++ emacs/compile.sh
@@ -0,0 +1,15 @@
+# Expands the archive
+tar xvf emacs-24.3.tar.xz
+cd emacs-24.3
+
+# Configure and compile
+./configure --prefix=/usr/local --with-gif=no
+make -j3
+make DESTDIR=/tmp/emacs install
+
+# Preparo il file desktop
+echo X-FullPathIcon=/usr/local/share/icons/hicolor/128x128/apps/emacs.png >> /tmp/emacs/usr/local/share/applications/emacs.desktop
+
+# Impacchetto
+cd ..
+mksquashfs /tmp/emacs emacs.tcz
