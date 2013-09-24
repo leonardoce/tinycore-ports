07eb8a9f6dd69a0a6145efec98e4dbbb6922dfc8
Leonardo Cecchi <leonardoce@interfree.it>
2013-09-24 13:41:59 +0000

    openssl, pop, smtp, imap support

--- mutt/compile.sh
+++ mutt/compile.sh
@@ -4,11 +4,13 @@ tar xvzf mutt-1.5.21.tar.gz
 tce-load -i perl5
 tce-load -i libidn-dev
 tce-load -i ncurses-dev
+tce-load -i openssl-1.0.0-dev
 
 cd mutt-1.5.21
-./configure --prefix=/usr/local --with-homespool
+./configure --prefix=/usr/local --with-homespool --enable-smtp --enable-imap --enable-pop --with-ssl
 make || exit 1
 make install DESTDIR=/tmp/mutt 
 cd ..
 
+rm -f mutt.tcz
 mksquashfs /tmp/mutt mutt.tcz
