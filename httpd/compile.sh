e78ce6c7829c95ee9cfe6021a7ca1cd34185fa93
Leonardo Cecchi <leonardoce@interfree.it>
2013-10-02 08:38:25 +0000

    httpd extension

--- httpd/compile.sh
+++ httpd/compile.sh
@@ -1,7 +1,7 @@
 . ../functions.sh
 
-tce-load -i libapr-dev.tcz
-tce-load -i libapr-util-dev.tcz
+tce-load -i apr-dev.tcz
+tce-load -i apr-util-dev.tcz
 
 tar xvf httpd-2.4.6.tar.bz2
 cd httpd-2.4.6
