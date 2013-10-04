6530ceb924502b1ea86f0064aa4f8df76ac96b8d
Leonardo Cecchi <leonardoce@interfree.it>
2013-10-04 09:32:00 +0000

    makeself tcz package

--- makeself/compile.sh
+++ makeself/compile.sh
@@ -1,4 +1,5 @@
 sh makeself.run
 mkdir -p /tmp/makeself/usr/local/bin
 cp makeself-2.1.5/*.sh /tmp/makeself/usr/local/bin
+cp makeself-2.1.5/makeself.sh /tmp/makeself/usr/local/bin/makeself
 mksquashfs /tmp/makeself makeself.tcz
